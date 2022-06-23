e7e45632df01fe07f5249bfc52b30dd44a0cf66f
Anandk <anand.k4756@gmail.com>
2022-06-23 23:13:30 +0530

    todo home page ready

--- lib/Screens/tasks_screen.dart
+++ lib/Screens/tasks_screen.dart
@@ -1,36 +1,61 @@
-import 'dart:html';
-
-import 'package:flutter/material.dart';
-
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueAccent,
-        body: Container(
-          padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                child: Icon(
-                  Icons.list,
-                  size: 30.0,
-                ),
-                backgroundColor: Colors.white,
-                radius: 30.0,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[500],
+          onPressed: () {
+            print("clicked");
+          },
+          child: Icon(Icons.add),
+        ),
+        backgroundColor: Colors.red,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40.0, left: 30.0, right: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.red,
+                      size: 30.0,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "Your Todo",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  )
+                ],
               ),
-              Text(
-                "Your Todo App",
-                style: TextStyle(
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700),
-              )
-            ],
-          ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    )),
+              ),
+            )
+          ],
         ));
   }
 }
